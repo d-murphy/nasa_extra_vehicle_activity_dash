1989f22793d9f41fe785344e47ba002e94421dbf
d-murphy <murphy.dan.s@gmail.com>
2021-05-03 00:54:18 -0400

    implementing first axis and tooltip

--- src/Plots/TimelineDotChart.js
+++ src/Plots/TimelineDotChart.js
@@ -1,8 +1,12 @@
 import * as d3 from 'd3'
 import './TimelineDotChart.css'
 import { useResizeDetector } from 'react-resize-detector'
+import { useEffect, useRef } from 'react'
 
 const TimelineDotChart = ({filterMissionData}) => {
+
+    const xAxisRef = useRef(null)
+    const yAxisRef = useRef(null)
 
     let TimelineDotData = filterMissionData
     let year = 0; // a year before the dataset
@@ -30,15 +34,49 @@ const TimelineDotChart = ({filterMissionData}) => {
         // .domain([d3.min(TimelineDotData, d=>d.year),
         //          d3.max(TimelineDotData, d=>d.year)])
         .range([margin.left, widthRs-margin.right])
+
+    const xAxis = d3.axisBottom().scale(xScale)
+    
     const yScale = d3
         .scaleLinear()
         .domain([0,maxNumInYear])
 //        .domain([0,d3.max(TimelineDotData, d=>d.yearRank)])
         .range([height-margin.bottom, margin.top])
 
+    const yAxis = d3.axisLeft().scale(yScale)
+
+    useEffect(() =>{
+        d3.select(xAxisRef.current).call(xAxis) //.call(xAxis)
+        
+        d3.selectAll('circle')
+            // .on('mouseover', function(event, d) {
+            //     console.log(event.srcElement.attributes.message.nodeValue)
+            // })
+            .on('mouseover', function(event, d) {
+                var tt1X = 200  
+                var tt1Y = 200 
+                d3.select("#testMouseover")
+                  .style("left", tt1X + "px")
+                  .style("top", tt1Y + "px")
+                  .html(event.srcElement.attributes.message.nodeValue)
+                  .classed("hidden", false)
+            })
+            .on("mouseout", function() {
+                d3.select("#testMouseover").classed("hidden", true)
+            })
+
+
+      //  console.log( d3.select(xAxisRef) )
+        //console.log(xAxis)
+    })
+
+
+
     return(
         <div className="TimelineDotChart" ref={ref}>
+            <div id="testMouseover">starter text</div>
             <svg viewBox={`0 0 ${widthRs} ${height}`}>
+
                 <g >
                     {TimelineDotData.map((d,i) => {
                         return(
@@ -47,9 +85,14 @@ const TimelineDotChart = ({filterMissionData}) => {
                                 cy={yScale(d.yearRank)}
                                 r={(xScale(1966)-xScale(1965))/2}
                                 fill={d.country==='USA' ? '#5599FF' : '#FF9955'}
+                                message={`<p>Check divs you did in other project, maybe use the same ${d.year}</p><p>${d.country}</p>`}
                             />
                         )
                     })}
+                </g>
+                <g>
+                    <g ref={xAxisRef} />
+                    <g ref={yAxisRef} />
                 </g>
             </svg>            
         </div>
